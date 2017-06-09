1059fcaa10d8e61ec1421b0faf45ef8bb68a5b98
mbp1225 <matheus698@hotmail.com>
2017-06-08 22:36:11 -0300

    Basic fps controller stuff.
    You move in the direction you are looking, including up and down. Gotta fix that.

---
+++ Assets/Scripts/fpsController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsController : MonoBehaviour {
+
+    public Camera viewCamera;
+
+    // Floats for mouse and player movement.
+    private float mouseX;
+    private float mouseY;
+    private float movH;
+    private float movV;
+
+    public float movementSpeed;
+    public float turnSpeed;
+
+    private Rigidbody rb;
+
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        //Getting the axis' movements.
+        movH = Input.GetAxis("Horizontal");
+        movV = Input.GetAxis("Vertical");
+
+        //Getting the mouse movement.
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        //Moving the player.
+        if (movH != 0 | movV != 0)
+        {
+            rb.velocity = (viewCamera.transform.forward * movV * movementSpeed) + (viewCamera.transform.right * movH * movementSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+
+        //Moving the camera.
+        viewCamera.transform.localEulerAngles += new Vector3(mouseY * -turnSpeed, mouseX * turnSpeed, 0);
+        //viewCamera.transform.rotation = Quaternion(Mathf.Clamp(viewCamera.transform.localEulerAngles.x, -90, 80), viewCamera.transform.localEulerAngles.y, viewCamera.transform.localEulerAngles.z);
+    }
+}
